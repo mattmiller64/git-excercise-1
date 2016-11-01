43c286b1fa7c152866b628790cd82c23f044a16c
DESKTOP-0H6OKBC\Matt <miller.matthew64@gmail.com>
2016-10-31 19:13:46 -0500

    Add project files.

---
+++ gitExercise1/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitExercise1
+{
+    class MyClass
+    {
+        public static void hello()
+        {
+            Console.WriteLine("method 'hello' says: hello");
+        }
+    }
+}
